9f7879b98eb6e1b9836d79286491632825452012
Raphael Capeto <61842405+rcapeto@users.noreply.github.com>
2022-12-19 14:20:09 -0300

    Adicionando arqvuivos bases de configuração

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+   bracketSpacing: true,
+   jsxBracketSameLine: true,
+   singleQuote: true,
+   trailingComma: 'all',
+   arrowParens: 'avoid',
+   options: {
+     parser: "babel-ts"
+   }
+ };
+ 
